08d5fc31e361c6bbd867b6f57227bc1489767cbf
tnChris <clbatis@newmex.com>
2017-11-07 11:44:28 -0700

    Branch for maintaining powerdns recursor docker image configuration.

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,9 @@
 FROM alpine
 MAINTAINER Chris Batis <clbatis@taosnet.com>
 
-RUN apk --no-cache add pdns pdns-backend-sqlite3 sqlite
-
-COPY pdns /etc/pdns/
+RUN apk --no-cache add pdns-recursor
 COPY docker-pdns.sh /docker-pdns.sh
 
 ENTRYPOINT ["/docker-pdns.sh"]
 
 EXPOSE 53/udp 53/tcp 8081
-
-ENV BACKEND=sqlite
--- docker-pdns.sh
+++ docker-pdns.sh
@@ -1,40 +1,24 @@
 #!/bin/sh
 
-# Initial setup
-if ! [ -e /etc/pdns/conf.d/backend.conf ]; then
-	if [ -n "$BACKEND" ]; then
-		if [ "$BACKEND" = "sqlite" ]; then
-			mv /etc/pdns/default.conf.d/sqlite.conf /etc/pdns/conf.d/backend.conf
-			chown -R pdns.pdns /etc/pdns/db
-		fi
-	fi
-	if [ -n "$APIKEY" ]; then
-		sed "s/DNSAPIS3rv3r/$APIKEY/" /etc/pdns/default.conf.d/api.conf >/etc/pdns/conf.d/api.conf
-		if [ -n "$WEBKEY" ]; then
-			sed -i "s/DNSW3bP4ssword/$WEBKEY/" /etc/pdns/conf.d/api.conf
-		fi
-	fi
-	if [ -n "$MASTER" ] && [ "$MASTER" = "yes" ]; then
-		mv /etc/pdns/default.conf.d/master.conf /etc/pdns/conf.d/master.conf
-		if [ -n "$AXFR" ]; then
-			echo allow-axfr-ips="$AXFR" >>/etc/pdns/conf.d/master.conf
-		fi
-	fi
-	# Defaults to slave mode
-	if [ -z "$SLAVE" ] || [ "$SLAVE" = "yes" ]; then
-		mv /etc/pdns/default.conf.d/slave.conf /etc/pdns/conf.d/slave.conf
-	fi
-	if [ -n "$SMTP" ]; then
-		sed "s/default-soa-mail=DIRECTOR/default-soa-mail=$SMTP/" </etc/pdns/default.conf.d/soa.conf >/etc/pdns/conf.d/soa.conf
-	fi
-	if [ -n "$SOA_NAME" ]; then
-		echo "default-soa-name=$SOA_NAME" >>/etc/pdns/conf.d/soa.conf
+echo 'daemon=no
+local-port=53
+local-address=0.0.0.0' >/etc/pdns/recursor.conf
+if [ -n "$ALLOW" ]; then
+	echo "allow-from=127.0.0.0/8,$ALLOW" >>/etc/pdns/recursor.conf
+fi
+if [ -n "$FORWARD" ]; then
+	echo "forward-zones=$FORWARD" >>/etc/pdns/recursor.conf
+fi
+if [ -n "$APIKEY" ]; then
+	echo "api-key=$APIKEY" >>/etc/pdns/recursor.conf
+fi
+if [ -n "$WEBKEY" ]; then
+	echo "webserver=yes" >>/etc/pdns/recursor.conf
+	echo "webserver-password=$WEBKEY" >>/etc/pdns/recursor.conf
+	echo "webserver-address=0.0.0.0" >>/etc/pdns/recursor.conf
+	echo "webserver-port=8081" >>/etc/pdns/recursor.conf
+	if [ -n "$WEBFROM" ]; then
+		echo "webserver-allow-from=$WEBFROM" >>/etc/pdns/recursor.conf
 	fi
 fi
-
-if ! [ -e /etc/pdns/conf.d/backend.conf ]; then
-	echo Must specify a backend to use!
-	exit 1
-fi
-
-/usr/sbin/pdns_server --config-dir=/etc/pdns
+/usr/sbin/pdns_recursor
